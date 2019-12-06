1b6f133391b157162c53ec8735e2d6ba8f825143
ststeiger <quandary82@hailmail.net>
2019-12-06 17:18:38 +0100

    Basic microphone test

---
+++ MicrophoneTest/Code/ExtensionContentTypeProvider.cs
@@ -0,0 +1,81 @@
+﻿
+namespace MicrophoneTest
+{
+
+
+    public class ExtensionContentTypeProvider
+        : Microsoft.AspNetCore.StaticFiles.IContentTypeProvider
+    {
+        // private static readonly ILog Log; 
+
+        private readonly Microsoft.AspNetCore.StaticFiles.IContentTypeProvider m_defaultContentTypeProvider;
+        private readonly System.Collections.Generic.Dictionary<string, string> m_mappings;
+
+
+        public ExtensionContentTypeProvider()
+        {
+            // this.Log = LogManager.GetLogger(typeof(ExtensionContentTypeProvider));
+            this.m_defaultContentTypeProvider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
+
+            this.m_mappings = new System.Collections.Generic.Dictionary<string, string>(
+                System.StringComparer.OrdinalIgnoreCase)
+            {
+                { "json", "application/json" },
+                { "jbc", "application/json" },
+                { "ta", "application/json" },
+                { "mustache", "text/plain" }
+            };
+        }
+
+
+        /// <summary>
+        /// Maps an extension to a content type.
+        /// </summary>
+        /// <param name="extension">The extension to map, without a leading dot.</param>
+        /// <param name="contentType">The content type to map to the extension.</param>
+        public void AddMapping(string extension, string contentType)
+        {
+            if (!this.m_mappings.ContainsKey(extension))
+                this.m_mappings.Add(extension, contentType);
+        }
+
+
+        private static string AfterLast(string value, string separator)
+        {
+            int index = value.LastIndexOf(separator);
+
+            if (index == -1)
+                return value;
+
+            return value.Substring(index + separator.Length);
+        }
+
+
+        bool Microsoft.AspNetCore.StaticFiles.IContentTypeProvider.
+            TryGetContentType(string subpath, out string contentType)
+        {
+            string extension = AfterLast(subpath, ".");
+
+            if (this.m_mappings.TryGetValue(extension.ToLowerInvariant(), out contentType))
+            {
+                return true;
+            }
+
+            if (this.m_defaultContentTypeProvider.TryGetContentType(subpath, out contentType))
+            {
+                return true;
+            }
+
+            // Log.WarnFormat("Unknown file type. {0}", subpath);
+            // contentType = null;
+            // return false;
+
+            contentType = "application/octet-stream";
+            return true;
+        }
+
+
+    } // End Class ExtensionContentTypeProvider 
+
+
+} // End Namespace AnySqlWebAdmin 
--- MicrophoneTest/Startup.cs
+++ MicrophoneTest/Startup.cs
@@ -46,7 +46,63 @@ namespace MicrophoneTest
                 app.UseExceptionHandler("/Home/Error");
             }
 
-            app.UseStaticFiles();
+
+            app.UseDefaultFiles(new DefaultFilesOptions()
+            {
+                DefaultFileNames = new System.Collections.Generic.List<string>()
+                {
+                   "index.htm", "index.html"
+                }
+            });
+
+
+
+            // https://stackoverflow.com/questions/38231739/how-to-disable-browser-cache-in-asp-net-core-rc2
+            // https://stackoverflow.com/questions/33342643/how-does-javascript-version-asp-append-version-work-in-asp-net-core-mvc
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                ServeUnknownFileTypes = true,
+                DefaultContentType = "application/octet-stream",
+                ContentTypeProvider = new MicrophoneTest.ExtensionContentTypeProvider(),
+
+                OnPrepareResponse = delegate (Microsoft.AspNetCore.StaticFiles.StaticFileResponseContext context)
+                {
+                    // https://stackoverflow.com/questions/49547/how-do-we-control-web-page-caching-across-all-browsers
+
+                    // The Cache-Control is per the HTTP 1.1 spec for clients and proxies
+                    // If you don't care about IE6, then you could omit Cache-Control: no-cache.
+                    // (some browsers observe no-store and some observe must-revalidate)
+                    context.Context.Response.Headers["Cache-Control"] =
+                        "no-cache, no-store, must-revalidate, max-age=0";
+                    // Other Cache-Control parameters such as max-age are irrelevant 
+                    // if the abovementioned Cache-Control parameters (no-cache,no-store,must-revalidate) are specified.
+
+
+                    // Expires is per the HTTP 1.0 and 1.1 specs for clients and proxies. 
+                    // In HTTP 1.1, the Cache-Control takes precedence over Expires, so it's after all for HTTP 1.0 proxies only.
+                    // If you don't care about HTTP 1.0 proxies, then you could omit Expires.
+                    context.Context.Response.Headers["Expires"] = "-1, 0, Tue, 01 Jan 1980 1:00:00 GMT";
+
+                    // The Pragma is per the HTTP 1.0 spec for prehistoric clients, such as Java WebClient
+                    // If you don't care about IE6 nor HTTP 1.0 clients 
+                    // (HTTP 1.1 was introduced 1997), then you could omit Pragma.
+                    context.Context.Response.Headers["pragma"] = "no-cache";
+
+
+                    // On the other hand, if the server auto-includes a valid Date header, 
+                    // then you could theoretically omit Cache-Control too and rely on Expires only.
+
+                    // Date: Wed, 24 Aug 2016 18:32:02 GMT
+                    // Expires: 0
+
+                    // But that may fail if e.g. the end-user manipulates the operating system date 
+                    // and the client software is relying on it.
+                    // https://stackoverflow.com/questions/21120882/the-date-time-format-used-in-http-headers
+                }
+
+            });
+
+
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
@@ -56,5 +112,9 @@ namespace MicrophoneTest
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
-    }
+
+
+    } // End Class 
+
+
 }
